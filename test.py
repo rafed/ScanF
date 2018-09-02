bb3ca28e672941a4990ef142d4098816735fdcd1
Rafed <rafed123@yahoo.com>
2018-09-03 05:06:09 +0600

    Saving screenshot

--- test.py
+++ test.py
@@ -11,11 +11,13 @@ chrome_options.add_argument("--start-maximized")
 driver = webdriver.Chrome(executable_path='./chromedriver', chrome_options=chrome_options)
 
 driver.get('http://www.google.com/');
-time.sleep(2) # Let the user actually see something!
-search_box = driver.find_element_by_name('q')
-search_box.send_keys('Rafed Yasir')
-search_box.submit()
-time.sleep(2) # Let the user actually see something!
+# time.sleep(2) # Let the user actually see something!
+# search_box = driver.find_element_by_name('q')
+# search_box.send_keys('Rafed Yasir')
+# search_box.submit()
+# time.sleep(2) # Let the user actually see something!
+
+driver.save_screenshot('shot.png')
 
 html = driver.page_source
 print(html)
