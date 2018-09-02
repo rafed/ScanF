6aa8a44c8e1b2f3d01d358d33e2d88a364acd7d6
Rafed <rafed123@yahoo.com>
2018-09-03 05:15:23 +0600

    Fixed headless screen size problem

--- test.py
+++ test.py
@@ -5,13 +5,15 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 chrome_options = Options()
+chrome_options.add_argument("window-size=1366,768");
+# chrome_options.add_argument("--start-maximized") # doesnt work for headless, go with window-size
 chrome_options.add_argument("--headless")  
-chrome_options.add_argument("--start-maximized")
+
 
 driver = webdriver.Chrome(executable_path='./chromedriver', chrome_options=chrome_options)
 
 driver.get('http://www.google.com/');
-# time.sleep(2) # Let the user actually see something!
+time.sleep(1) # Let the user actually see something!
 # search_box = driver.find_element_by_name('q')
 # search_box.send_keys('Rafed Yasir')
 # search_box.submit()
